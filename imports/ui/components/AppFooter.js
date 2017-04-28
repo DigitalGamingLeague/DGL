387d8ce67f483026f8d746af431e4f66237f9548
DigitalGamingLeague <rdanielcurry@gmail.com>
2017-04-28 19:50:05 -0400

    Added footer

--- imports/ui/components/AppFooter.js
+++ imports/ui/components/AppFooter.js
@@ -7,7 +7,62 @@ import container from '../../modules/container';
 import FontAwesome from 'react-fontawesome';
 
 const AppFooter = ({ hasUser }) => (
-    <div>hi</div>           
+    <footer>
+       <div class="container">
+
+            <div className="row">
+                <div id="footer-branding" className="col-md-4">
+                    <img src="/images/logo.svg" />
+                    <p>
+                        Copyright &copy; 2017 The Digital Gaming League.
+                        <br />
+                        All Rights Reserved.
+                    </p>
+                </div> 
+
+                <div className="col-md-2">
+                    <header>Questions?</header>
+                    <ul>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                        <li>Staff</li>
+                        <li>Ban List</li>
+                    </ul>
+                </div>
+    
+                <div className="col-md-2">
+                    <header>Social</header>
+                    <ul>
+                        <li>Twitch</li>
+                        <li>Twitter</li>
+                        <li>Facebook</li>
+                        <li>YouTube</li>
+                        <li>Steam Group</li>
+                    </ul>
+                </div>
+
+                <div className="col-md-2">
+                    <header>Events</header>
+                    <ul>
+                        <li>NA Season 9</li>
+                        <li>EU Season 9</li>
+                        <li>AU/NZ Season 9</li>
+                        <li>LHL Season 26</li>
+                        <li>RSL Season 25</li>
+                    </ul>
+                </div>
+
+                <div className="col-md-2">
+                    <header>Partners</header>
+                    <ul>
+                        <li>Oorah Gaming</li>
+                    </ul>
+                </div>
+    
+            </div>
+
+        </div>
+    </footer>           
 );
 
 AppFooter.propTypes = {
