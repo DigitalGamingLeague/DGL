944309e90485c22a4b8224989f6adfcb118b71d9
Recidivist <rdanielcurry@gmail.com>
2017-05-04 22:01:22 +0000

    Pushing updated copy of develop branch

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 const Index = () => (
@@ -7,34 +8,79 @@ const Index = () => (
     
         <section id="home-banner">
     
-            <Carousel 
-                prevIcon={<FontAwesome name="chevron-left" size="5x" />}
-                nextIcon={<FontAwesome name="chevron-right" size="5x" />}>
-                <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="/images/genericpicture.jpg"/>
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="/images/carousel.png"/>
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img width={900} height={500} alt="900x500" src="/images/carousel.png"/>
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <Row>
+    
+                <Col md={10} mdOffset={1}>
+    
+                    <Carousel 
+                        prevIcon={<FontAwesome name="chevron-left" size="5x" />}
+                        nextIcon={<FontAwesome name="chevron-right" size="5x" />}>
+                        <Carousel.Item>
+                            <img src="/images/genericpicture.jpg"/>
+                        </Carousel.Item>
+                    </Carousel>
+    
+                </Col>
+    
+            </Row>
+    
+        </section>
+    
+        <section id="home-results">
+
+            <Row>
+
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                <MatchResult />
+                
+                <Col md={2}>
+                    <FontAwesome name="level-up" size="3x" />
+                </Col>
+
+            </Row>
+		
+        </section>
+    
+        <section id="home-updates">
+    
+            <Row>
+
+                <Col md={8} id="news">
+
+                    <h4>Update from 1.6</h4>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae ornare est, a finibus ex. Suspendisse a dolor ut mauris rhoncus sagittis vel nec diam. Cras nec tempus nulla, nec pellentesque lacus. Ut ligula velit, viverra ut libero sit amet, ullamcorper aliquam ante. Ut eu tellus tincidunt, dapibus velit vitae, auctor diam. Duis augue urna, semper ornare tempus vel, pharetra vitae diam. Suspendisse pellentesque tempor quam sed tincidunt. Proin ut venenatis nunc. Morbi at lorem rutrum, ultricies turpis eu, placerat risus. Maecenas vitae molestie felis, aliquam molestie dolor. Suspendisse potenti. Quisque vitae dolor a tellus blandit rutrum ac euismod urna. Donec et lorem lacinia, tincidunt sem vel, sagittis ante. Nam turpis enim, ornare ut enim in, dapibus porta nisl.
+
+                    Maecenas efficitur nunc vel elit consectetur, venenatis accumsan ligula rhoncus. Donec eleifend metus lectus, nec volutpat justo egestas sit amet. Nam euismod magna ut enim aliquet scelerisque. Sed ante metus, sagittis sed blandit sed, gravida et nulla. Vivamus interdum ligula vitae ipsum lacinia porttitor. Suspendisse sit amet odio turpis. Quisque libero enim, consequat a est quis, hendrerit hendrerit ligula. In ex enim, dictum id rutrum vel, pulvinar quis massa. Aliquam diam nibh, auctor ac blandit eget, bibendum ultricies lectus. Mauris semper faucibus erat, ac vestibulum eros. Curabitur libero risus, rhoncus vitae ex non, iaculis fermentum magna. Aliquam finibus metus ligula, in volutpat magna finibus non. Donec arcu ligula, pharetra vel urna eu, rutrum malesuada risus.
+
+                    Phasellus et auctor arcu. Suspendisse at eleifend diam. Duis ultrices dolor id nibh vulputate, quis sagittis metus viverra. In venenatis felis ac dapibus posuere. Donec semper at nunc nec tempor. Suspendisse tincidunt fermentum elit. Morbi vel nulla quam. Suspendisse nulla nulla, consequat a ultricies sit amet, tristique pharetra eros. Nam vestibulum odio a augue commodo venenatis. Quisque interdum facilisis mi, vel porttitor ipsum faucibus vel. Sed vitae vulputate justo, vitae viverra arcu. Aliquam eget tempor felis, tristique lobortis nibh. In hac habitasse platea dictumst. Vestibulum massa lacus, semper in urna vitae, bibendum pulvinar justo. Cras sit amet volutpat turpis, quis ornare nisi.
+
+                </Col>
+
+                <Col md={4} id="twitter">
+
+                    Twitter
+
+                </Col>
+    
+            </Row>
     
         </section>
   </div>
 );
+                               
+const MatchResult = () => (
 
+    <Col md={2} className="match-result">
+        <header>
+            TooL vs Rekt    
+        </header>
+        <div>
+            hello world
+        </div>
+    </Col>
+);
 export default Index;
