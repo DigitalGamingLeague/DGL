c3dc0a80b79564aa78e348b069bf6548e982466b
Aita <rdanielcurry@gmail.com>
2017-05-08 14:21:09 +0000

    Twitter backend added

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -2,3 +2,4 @@ import { BrowserPolicy } from 'meteor/browser-policy-common';
 // e.g., BrowserPolicy.content.allowOriginForAll( 's3.amazonaws.com' );
 BrowserPolicy.content.allowStyleOrigin('fonts.googleapis.com');
 BrowserPolicy.content.allowFontOrigin('fonts.gstatic.com');
+BrowserPolicy.content.allowOriginForAll('*.twitter.com');
--- server/main.js
+++ server/main.js
@@ -1,1 +1,15 @@
 import '/imports/startup/server';
+import Twitter from 'twitter';
+
+var twitterClient = new Twitter({
+    consumer_key: Meteor.settings.private.twitter.consumer_key,
+    consumer_secret: Meteor.settings.private.twitter.consumer_secret,
+    access_token_key: Meteor.settings.private.twitter.access_token_key,
+    access_token_secret: Meteor.settings.private.twitter.access_token_secret
+});
+
+twitterClient.get('statuses/user_timeline', function(error, tweets, response) {
+  if(error) throw error;
+  console.log(tweets);  // The favorites. 
+  //console.log(response);  // Raw response object. 
+});
